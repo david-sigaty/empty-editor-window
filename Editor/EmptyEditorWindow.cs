df0e8786db64c6cb468c594058264738439d7595
Day V <pinao212@gmail.com>
2020-06-15 14:19:23 -0600

    Initial commit.

---
+++ Editor/EmptyEditorWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EmptyEditorWindow : EditorWindow
+{
+	[MenuItem("Window/Empty")]
+	public static void Open()
+	{
+		EmptyEditorWindow window = CreateInstance<EmptyEditorWindow>();
+		window.titleContent = new GUIContent("Empty");
+		window.Show();
+	}
+}
